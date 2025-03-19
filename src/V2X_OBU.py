731b9506f5ae0c02d600257bfb2c983a0d468fcd
John Chrosniak <chrosniakj@gmail.com>
2025-03-19 09:34:51 -0400

    addressed comments

--- src/V2X_OBU.py
+++ src/V2X_OBU.py
@@ -130,6 +130,7 @@ def sendLAN(lPacket):
 
 def checkMessageType(message):
 	try:
+		# Look for the text after "Type=" in the message
 		match = re.search(rb'Type=([A-Za-z0-9]+)', message)
 	except Exception as e:
 		v2x_logger.info("%s", e)
@@ -158,9 +159,9 @@ def VANET_listening_thread():
 						v2x_logger.info("Received ack")
 					elif message_type in {"MobilityOperation", "TMSG03"}:
 						if data == previous_packet_received:  # Duplicate message received, so just resend ack
-							v2x_logger.info("Received duplicate message, resending ack")
+							v2x_logger.info("Received duplicate MOM, resending ack")
 						else:  # New message received, forward it to LAN and send ack
-							v2x_logger.info("Received new message, sent ack")
+							v2x_logger.info("Received new MOM, sent ack")
 							sendLAN(pkt[0])
 						previous_packet_received = data
 						sendVANET(ack)
@@ -202,7 +203,7 @@ def LAN_listening_thread():
 					if message_type in {"MobilityOperation", "TMSG03"}:
 						# Wait for ack
 						waiting_for_ack = True
-						v2x_logger.info("Message sent, waiting for ack")
+						v2x_logger.info("MOM sent, waiting for ack")
 						time.sleep(1.0)
 						for i in range(120):  # Attempt to rebroadcast for 2 minutes before giving up
 							with mutex:
